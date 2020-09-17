bd7ccfdce422576ce2d0a417692fb2f50925dc57
Christophe Thibaut <cthibaut@octo.com>
2020-09-17 20:06:15 +0200

    I/O words and tokenizer

--- Highlight.fs
+++ Highlight.fs
@@ -7,3 +7,35 @@ DEFER _KEY
 ' EMIT IS _EMIT
 ' TYPE IS _TYPE
 ' KEY  IS _KEY
+
+CREATE TOKEN 1024 ALLOT
+
+: .COLOR ( c -- )
+    27 _EMIT
+    [CHAR] [ _EMIT
+    [CHAR] 3 _EMIT
+    [CHAR] 0 + _EMIT
+    [CHAR] m _EMIT ;
+
+: IS-SPACE? ( c -- ? )
+    DUP  32 =
+    OVER 10 = OR
+    OVER 13 = OR
+    SWAP  9 = OR ;
+
+: .SKIP-SPACE ( -- c )
+    BEGIN
+        _KEY DUP 4 <> 
+        OVER IS-SPACE? AND WHILE
+        _EMIT
+    REPEAT ;
+
+: .>TOKEN ( -- # c )
+    0 .SKIP-SPACE
+    BEGIN
+        DUP 4 <> 
+        OVER IS-SPACE? 0= AND WHILE
+        OVER TOKEN + C!
+        1+
+        _KEY
+    REPEAT ;
--- Tests.fs
+++ Tests.fs
@@ -23,32 +23,75 @@ VARIABLE _#INPUT
         4
     THEN ;
 
+: RESET-OUTPUT 
+    _OUTPUT# OFF ;
+
+: SET-INPUT ( addr # -- )
+    _INPUT# OFF 
+    _#INPUT ! 
+    _INPUT _#INPUT @ CMOVE ;
+
+: ?OUTPUT ( addr # -- ? )
+    _OUTPUT _OUTPUT# @
+    COMPARE 0 ?S ;
+    
+' EMIT>! IS _EMIT
+' TYPE>! IS _TYPE
+' KEY<@ IS _KEY
+
 T{ ." _EMIT can be redirected for the sake of testing" cr
-   ' EMIT>! IS _EMIT
-   _OUTPUT# OFF
-   42 _EMIT
-   46 _EMIT
-   44 _EMIT
-   _OUTPUT# @ 3 ?S
-   S" *.," _OUTPUT _OUTPUT# @ COMPARE 0 ?S
+    RESET-OUTPUT
+   42 _EMIT 46 _EMIT 44 _EMIT
+   S" *.," ?OUTPUT
 }T
 
 T{ ." _TYPE can be redirected for the sake of testing" cr
-    ' TYPE>! IS _TYPE
-    _OUTPUT# OFF
+    _OUTPUT# OFF 
     S" Hello World!" _TYPE
-    S" Hello World!" _OUTPUT _OUTPUT# @ COMPARE 0 ?S
+    S" Hello World!" ?OUTPUT
 }T
 
 T{ ." _KEY can be redirected for the sake of testing" cr
-    ' KEY<@ IS _KEY
-    _INPUT# OFF
-    S" *.," _INPUT SWAP CMOVE
-    3 _#INPUT !
+    S" *.," SET-INPUT
     _KEY 42 ?S
     _KEY 46 ?S
     _KEY 44 ?S
     _KEY 4 ?S
 }T
 
+T{ ." .COLOR changes the terminal color" CR
+    RESET-OUTPUT
+    2 .COLOR
+    _OUTPUT C@ 27 ?S
+    _OUTPUT 1+ 4 S" [32m" COMPARE 0 ?S
+}T
+
+T{ ." .SKIP-SPACE prints input chars until non space found" cr
+    RESET-OUTPUT
+    S"    FOO" SET-INPUT
+    .SKIP-SPACE
+    CHAR F ?S
+    S"    " ?OUTPUT
+}T
+
+T{ ." .SKIP-SPACE prints input chars until non space found" cr
+    RESET-OUTPUT
+    S\"   \t\n\rFOO" SET-INPUT
+    .SKIP-SPACE
+    CHAR F ?S
+    S\"   \t\n\r" ?OUTPUT
+}T
+T{ ." .SKIP-SPACE yields EOF is no input chars anymore" cr
+    RESET-OUTPUT
+    S\"    " SET-INPUT
+    .SKIP-SPACE
+    4 ?S
+}T
+T{ ." .>TOKEN gets the next token from the input stream" cr
+    S"    FOO  " SET-INPUT
+    RESET-OUTPUT
+    .>TOKEN 32 ?S
+    TOKEN SWAP S" FOO" COMPARE 0 ?S
+    S"    " ?OUTPUT
+}T
 bye
