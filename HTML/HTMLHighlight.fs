3654ec1e1c9e4b428563b19219776b1e872b7b94
Christophe Thibaut <cthibaut@octo.com>
2020-09-24 10:40:37 +0200

    skip lines, strings, comment

--- HTML/HTMLHighlight.fs
+++ HTML/HTMLHighlight.fs
@@ -1,6 +1,11 @@
 #! /usr/local/bin/gforth
 
-\ DEFER the basic output and input words so that 
+ 4 CONSTANT EOF
+ 9 CONSTANT TAB
+10 CONSTANT NL
+13 CONSTANT CR%
+
+\ DEFER the basic output and input words so that
 \ we can substitute any word instead of emit type and key
 DEFER _EMIT
 DEFER _TYPE
@@ -12,24 +17,84 @@ DEFER _KEY
 ' TYPE IS _TYPE
 ' KEY  IS _KEY
 
-: to-string ( n -- addr c )  s>d <# # # # # # # #> ;
-
+\ convert a number to 6 digit hex string
 : HEX6 ( n -- addr # )
     BASE @ >R HEX
     s>d <# # # # # # # #>
     R> BASE ! ;
 
 \ display a span tag with color and font-weight
-: .<SPAN> ( rgb bold? -- )
+: <SPAN>. ( rgb bold? -- )
     S\" <span style=\"color:#" _TYPE
     SWAP HEX6 _TYPE
     S\" ; font-weight:" _TYPE
     IF S\" bold" ELSE S\" normal" THEN _TYPE
     S\" ;\">" _TYPE ;
 \ "
-\ display a end of span tag
-: .</SPAN> 
+\ display an end of span tag
+: </SPAN>.
     S\" </span>" _TYPE ;
+
+\ display a pre tag with color and background
+: <PRE>. ( col col -- )
+    S\" <pre style=\"color:#" _TYPE
+    SWAP HEX6 _TYPE
+    S\" ;background:#" _TYPE
+    HEX6 _TYPE
+    S\" ;\">" _TYPE ;
+\ "
+\ display an end of pre tag
+: </PRE>.
+    S\" </pre>" _TYPE ;
+
+\ true if char is a space char
+: IS-SPACE? ( c -- ? )
+    DUP  BL  =
+    OVER NL  = OR
+    OVER CR% = OR
+    SWAP TAB = OR ;
+
+\ read and print chars until a non-space is met
+: SKIP-SPACE. ( -- c )
+    BEGIN
+        _KEY DUP EOF <>
+        OVER IS-SPACE? AND WHILE
+        _EMIT
+    REPEAT ;
+
+\ print a char if it's not eof
+: ?_EMIT ( c -- )
+    DUP EOF <> IF _EMIT ELSE DROP THEN ;
+
+\ read and print chars until end of line is met
+\ emit that end of line if it's not eof
+: SKIP-LINE. 
+    BEGIN
+        _KEY DUP  EOF <>
+             OVER NL  <> AND
+             OVER CR% <> AND WHILE
+        _EMIT
+    REPEAT ?_EMIT ;
+
+\ read and print chars until " or eol or eof
+\ emit that end of string if it's not eof
+: SKIP-STRING.
+    BEGIN
+        _KEY DUP  EOF <>
+             OVER NL  <> AND
+             OVER CR% <> AND
+             OVER [CHAR] " <> AND WHILE
+        _EMIT
+    REPEAT ?_EMIT ;
+
+\ read and print chars until ) or eof
+\ emit that end of string if it's not eof
+: SKIP-COMMENT.
+    BEGIN
+        _KEY DUP  EOF <>
+             OVER [CHAR] ) <> AND WHILE
+        _EMIT
+    REPEAT ?_EMIT ;
 
 [IFUNDEF] TESTING
 [THEN]
--- HTML/Tests.fs
+++ HTML/Tests.fs
@@ -56,15 +56,58 @@ VARIABLE _#INPUT
 ' TYPE>! IS _TYPE
 ' KEY<@ IS _KEY
 
-T{ ." .<SPAN> outputs a span tag with a color and a font weight" CR
-    RESET-OUTPUT 4807 FALSE .<SPAN>
+T{ ." <SPAN>. outputs a span tag with a color and a font weight" CR
+    RESET-OUTPUT 4807 FALSE <SPAN>.
     S\" <span style=\"color:#0012C7; font-weight:normal;\">" ?OUTPUT
-    RESET-OUTPUT 1024 TRUE .<SPAN>
+    RESET-OUTPUT 1024 TRUE <SPAN>.
     S\" <span style=\"color:#000400; font-weight:bold;\">" ?OUTPUT
 }T
-T{ ." .</SPAN> outputs a end span tag" CR
-    RESET-OUTPUT .</SPAN>
+T{ ." </SPAN>. outputs a end span tag" CR
+    RESET-OUTPUT </SPAN>.
     S\" </span>" ?OUTPUT
 }T
-
+T{ ." <PRE>. outputs a pre tag with a color and a background" CR
+    RESET-OUTPUT 1024 4807 <PRE>.
+    S\" <pre style=\"color:#000400;background:#0012C7;\">" ?OUTPUT
+}T
+T{ ." </PRE>. outputs a end pre tag" CR
+    RESET-OUTPUT </PRE>.
+    S\" </pre>" ?OUTPUT
+}T
+T{ ." SKIP-SPACE. prints input chars until non space found" CR
+    RESET-OUTPUT
+    S\"  \t\n\r  foo" SET-INPUT
+    SKIP-SPACE.
+    CHAR f ?S
+    S\"  \t\n\r  " ?OUTPUT
+}T
+T{ ." SKIP-SPACE. returns EOF if end of input stream" CR
+    RESET-OUTPUT
+    S\"    " SET-INPUT
+    SKIP-SPACE. 4 ?S
+}T
+T{ ." SKIP-STRING prints input chars until (and including) a double quote" CR
+    RESET-OUTPUT
+    S\" foo bar\" qux" SET-INPUT
+    SKIP-STRING.
+    S\" foo bar\"" ?OUTPUT
+}T
+T{ ." SKIP-LINE. prints input chars until end of line" CR
+    S\" foo bar \n\r" SET-INPUT
+    RESET-OUTPUT
+    SKIP-LINE.
+    S\" foo bar \n" ?OUTPUT
+}T
+T{ ." SKIP-LINE. prints input chars until end of input" CR
+    S\" foo" SET-INPUT
+    RESET-OUTPUT
+    SKIP-LINE.
+    S\" foo" ?OUTPUT
+}T
+T{ ." SKIP-COMMENT. prints input chars until (and including) right parens or end of input" CR
+    S\" foo\n bar) qux" SET-INPUT
+    RESET-OUTPUT
+    SKIP-COMMENT.
+    S\" foo\n bar)" ?OUTPUT
+}T
 BYE
